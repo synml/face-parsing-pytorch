0f971a21a6ec56140741f9af196485902cfbf75c
Seokyong Shin <kistssy@gmail.com>
2022-02-18 13:45:06 +0900

    celebamaskhq: Refactor to simplify code

--- datasets/celebamaskhq.py
+++ datasets/celebamaskhq.py
@@ -67,10 +67,12 @@ class CelebAMaskHQ(torchvision.datasets.VisionDataset):
         assert target_type in ('mask', 'pose', 'attr')
         self.split = split
         self.target_type = target_type
+        self.class_names = [cls.name for cls in self.classes]
+        self.colors = [cls.color for cls in self.classes]
+        self.num_classes = len(self.classes)
+        self.preprocessed_mask_path = os.path.join(self.root, 'preprocessed_mask')
         self.images = []
         self.targets = []
-        self.class_names, self.colors, self.num_classes = self._get_class_info()
-        self.preprocessed_mask_path = os.path.join(self.root, 'preprocessed_mask')
 
         if download:
             self.download()
@@ -107,18 +109,6 @@ class CelebAMaskHQ(torchvision.datasets.VisionDataset):
 
         self.images.sort(key=lambda x: int(os.path.splitext(os.path.basename(x))[0]))
         self.targets.sort(key=lambda x: int(os.path.splitext(os.path.basename(x))[0]))
-
-    def _get_class_info(self):
-        class_names = []
-        colors = []
-        num_classes = 0
-
-        for celebamaskhq_class in self.classes:
-            class_names.append(celebamaskhq_class.name)
-            colors.append(celebamaskhq_class.color)
-            num_classes += 1
-
-        return class_names, colors, num_classes
 
     def download(self):
         dataset_file = {'id': '1PtttcVHOjC5-9xSBPWNHh0OOUrEgHWcl',
