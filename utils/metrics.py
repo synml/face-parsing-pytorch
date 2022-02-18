6cc337d6232652aedb8d906f7dc592a8565604e4
Seokyong Shin <kistssy@gmail.com>
2022-02-18 14:06:25 +0900

    metrics: Refactor to change function order

--- utils/metrics.py
+++ utils/metrics.py
@@ -6,6 +6,17 @@ class Evaluator:
         self.confusion_matrix = torch.zeros((num_classes, num_classes), dtype=torch.int64, device=device)
         self.num_classes = num_classes
 
+    def update_matrix(self, gt_batch: torch.Tensor, pred_batch: torch.Tensor):
+        assert gt_batch.shape == pred_batch.shape
+        self.confusion_matrix += self._generate_matrix(gt_batch, pred_batch)
+
+    def _generate_matrix(self, gt_batch: torch.Tensor, pred_batch: torch.Tensor) -> torch.Tensor:
+        mask = (gt_batch >= 0) & (gt_batch < self.num_classes)
+        label = self.num_classes * gt_batch[mask] + pred_batch[mask]
+        count = torch.bincount(label, minlength=self.num_classes ** 2)
+        confusion_matrix = count.reshape(self.num_classes, self.num_classes)
+        return confusion_matrix
+
     def pixel_accuracy(self) -> torch.Tensor:
         acc = torch.diag(self.confusion_matrix).sum() / self.confusion_matrix.sum()
         return acc
@@ -27,20 +38,3 @@ class Evaluator:
         iou = self.intersection_over_union(percent)
         miou = torch.nanmean(iou)
         return iou, miou
-
-    def frequency_weighted_intersection_over_union(self) -> torch.Tensor:
-        freq = torch.sum(self.confusion_matrix, dim=0) / torch.sum(self.confusion_matrix)
-        iou = self.intersection_over_union()
-        fwiou = (freq[freq > 0] * iou[freq > 0]).sum()
-        return fwiou
-
-    def _generate_matrix(self, gt_batch: torch.Tensor, pred_batch: torch.Tensor) -> torch.Tensor:
-        mask = (gt_batch >= 0) & (gt_batch < self.num_classes)
-        label = self.num_classes * gt_batch[mask] + pred_batch[mask]
-        count = torch.bincount(label, minlength=self.num_classes ** 2)
-        confusion_matrix = count.reshape(self.num_classes, self.num_classes)
-        return confusion_matrix
-
-    def update_matrix(self, gt_batch: torch.Tensor, pred_batch: torch.Tensor):
-        assert gt_batch.shape == pred_batch.shape
-        self.confusion_matrix += self._generate_matrix(gt_batch, pred_batch)
