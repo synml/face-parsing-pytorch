e27f0587d455084193184fc7932e86c8f1362568
Seokyong Shin <kistssy@gmail.com>
2022-02-21 07:58:17 +0900

    metrics: Refactor to change function order

--- utils/metrics.py
+++ utils/metrics.py
@@ -6,16 +6,16 @@ class Evaluator:
         self.confusion_matrix = torch.zeros((num_classes, num_classes), dtype=torch.int64, device=device)
         self.num_classes = num_classes
 
-    def update_matrix(self, gt_batch: torch.Tensor, pred_batch: torch.Tensor):
-        assert gt_batch.shape == pred_batch.shape
-        self.confusion_matrix += self._generate_matrix(gt_batch, pred_batch)
-
     def _generate_matrix(self, gt_batch: torch.Tensor, pred_batch: torch.Tensor) -> torch.Tensor:
         mask = (gt_batch >= 0) & (gt_batch < self.num_classes)
         label = self.num_classes * gt_batch[mask] + pred_batch[mask]
         count = torch.bincount(label, minlength=self.num_classes ** 2)
         confusion_matrix = count.reshape(self.num_classes, self.num_classes)
         return confusion_matrix
+
+    def update_matrix(self, gt_batch: torch.Tensor, pred_batch: torch.Tensor):
+        assert gt_batch.shape == pred_batch.shape
+        self.confusion_matrix += self._generate_matrix(gt_batch, pred_batch)
 
     def pixel_accuracy(self) -> torch.Tensor:
         acc = torch.diag(self.confusion_matrix).sum() / self.confusion_matrix.sum()
