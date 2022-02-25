630a1a24939d3eae0bfbe86f1e8e951a60587dce
Seokyong Shin <kistssy@gmail.com>
2022-02-25 14:25:02 +0900

    demo_dataset: Add transforms

--- datasets/demo_dataset.py
+++ datasets/demo_dataset.py
@@ -10,8 +10,9 @@ class DemoDataset(torchvision.datasets.VisionDataset):
     def __init__(self,
                  root: str,
                  transform: Optional[Callable] = None,
-                 target_transform: Optional[Callable] = None):
-        super().__init__(root, transform, target_transform)
+                 target_transform: Optional[Callable] = None,
+                 transforms: Optional[Callable] = None):
+        super().__init__(root, transforms, transform, target_transform)
         self.images = glob.glob(os.path.join(self.root, '*'))
 
         try:
@@ -21,7 +22,12 @@ class DemoDataset(torchvision.datasets.VisionDataset):
 
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, None]:
         image = torchvision.io.read_image(self.images[index], torchvision.io.ImageReadMode.RGB)
-        return image, None
+        target = torch.zeros_like(image)
+
+        if self.transforms is not None:
+            image, target = self.transforms(image, target)
+
+        return image, target
 
     def __len__(self):
         return len(self.images)
