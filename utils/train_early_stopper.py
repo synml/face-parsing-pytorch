f2db51d17ea8ac38efd50793536b4f9b81bfccc8
Seokyong Shin <kistssy@gmail.com>
2022-03-02 16:33:34 +0900

    Fix train early stopper

--- utils/train_early_stopper.py
+++ utils/train_early_stopper.py
@@ -1,11 +1,11 @@
 def train_early_stopper():
-    with open('train_interupter.ini', 'r', encoding='utf-8') as f:
+    with open('train_early_stopper.ini', 'r', encoding='utf-8') as f:
         flag = f.read().strip()
 
     if flag == '0':
         return False
     elif flag == '1':
-        with open('train_interupter.ini', 'w', encoding='utf-8') as f:
+        with open('train_early_stopper.ini', 'w', encoding='utf-8') as f:
             f.write('0')
         return True
     else:
