8a9d74010484ffa43ec4d5ae2f56cc57110182e3
Seokyong Shin <kistssy@gmail.com>
2022-03-04 16:48:09 +0900

    sh: Add comment

--- train_ddp.sh
+++ train_ddp.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# You can easily run this after chmod with the following command: ./train_ddp.sh
 chmod u+x train_ddp.sh
+
+# Set the variable according to the environment of node for optimal performance.
 export OMP_NUM_THREADS=1
+
+# Set the variable according to the number of GPUs in the node.
 python -m torch.distributed.run --nproc_per_node=8 train.py
