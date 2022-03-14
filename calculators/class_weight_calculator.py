419ca6898ff2200d3b5492cac5d5fa1571e164bb
Seokyong Shin <kistssy@gmail.com>
2022-03-14 09:09:38 +0900

    calculators: 코드 수정

--- calculators/class_weight_calculator.py
+++ calculators/class_weight_calculator.py
@@ -1,19 +1,18 @@
 import torch
+import torch.utils.data
 from tqdm import tqdm
 
 
-def calculate_class_weight(dataloader, num_classes):
-    z = torch.zeros(num_classes)
-    for sample in tqdm(dataloader, desc='Calculate class weight'):
-        y = sample['label']
-        y = y.detach().cpu().numpy()
-        mask = (y >= 0) & (y < num_classes)
-        labels = y[mask].astype(torch.uint8)
+def calculate_class_weight(dataloader: torch.utils.data.DataLoader, num_classes: int):
+    frequencies = torch.zeros(num_classes)
+    for _, target in tqdm(dataloader, desc='Calculate class weight'):
+        mask = (target >= 0) & (target < num_classes)
+        labels = target[mask].astype(torch.uint8)
         count_l = torch.bincount(labels, minlength=num_classes)
-        z += count_l
-    total_frequency = torch.sum(z)
+        frequencies += count_l
+
     class_weights = []
-    for frequency in z:
-        class_weight = 1 / (torch.log(1.02 + (frequency / total_frequency)))
+    for frequency in frequencies:
+        class_weight = 1 / (torch.log(1.02 + (frequency / frequencies.sum())))
         class_weights.append(class_weight)
     return class_weights
