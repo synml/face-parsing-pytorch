20f4975eb8b0eda93bd5ac66f888fc9169c23af2
Seokyong Shin <kistssy@gmail.com>
2022-03-23 17:47:36 +0900

    class_weight: Change algorithm

--- calculators/class_weight.py
+++ calculators/class_weight.py
@@ -3,15 +3,12 @@ import torch.utils.data
 from tqdm import tqdm
 
 
-def calculate_class_weight(dataloader: torch.utils.data.DataLoader, num_classes: int):
-    frequencies = torch.zeros(num_classes)
+def calculate_class_weight(dataloader: torch.utils.data.DataLoader, num_classes: int) -> torch.Tensor:
+    counts = torch.zeros(num_classes)
     for _, target in tqdm(dataloader, desc='Calculate class weight'):
         target = target.to(torch.uint8)
-        count_l = torch.bincount(target.flatten(), minlength=num_classes)
-        frequencies += count_l
+        count = torch.bincount(target.flatten(), minlength=num_classes)
+        counts += count
 
-    class_weights = []
-    for frequency in frequencies:
-        class_weight = 1 / (torch.log(1.02 + (frequency / frequencies.sum())))
-        class_weights.append(class_weight)
-    return class_weights
+    weight = torch.median(counts) / counts
+    return weight
