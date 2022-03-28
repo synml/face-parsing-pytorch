bbca9b649c877ff1a10d8e96d2f85d34e7c585d9
Seokyong Shin <kistssy@gmail.com>
2022-03-28 11:09:31 +0900

    test: Add columm

--- models/test.py
+++ models/test.py
@@ -8,7 +8,7 @@ def test_model(model: nn.Module, input_size: tuple[int, int, int, int], graph_di
     model.eval()
 
     model_statistics = torchinfo.summary(model, input_size, depth=10,
-                                         col_names=('kernel_size', 'output_size', 'num_params', 'mult_adds'),
+                                         col_names=('input_size', 'kernel_size', 'output_size', 'num_params', 'mult_adds'),
                                          row_settings=('depth', 'var_names'))
     print(f'Total GFLOPs: {model_statistics.total_mult_adds * 2 / 1e9:.4f}')
 
