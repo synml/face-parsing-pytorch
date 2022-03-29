533d5daad7b12e838ffa98692c15043605bfd0e9
Seokyong Shin <kistssy@gmail.com>
2022-03-29 19:41:25 +0900

    lane: Change class color

--- datasets/lane.py
+++ datasets/lane.py
@@ -18,7 +18,7 @@ class Lane(torchvision.datasets.VisionDataset):
     classes = [
         LaneClass('background', 0, (0, 0, 0)),
         LaneClass('white_lane', 1, (255, 255, 255)),
-        LaneClass('yellow_lane', 2, (255, 200, 0)),
+        LaneClass('yellow_lane', 2, (255, 255, 0)),
         LaneClass('stop_line', 3, (255, 0, 0)),
     ]
 
