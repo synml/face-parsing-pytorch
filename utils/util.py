ea968a37f442c66475ef060e09a66ec7aeab1e45
Seokyong Shin <kistssy@gmail.com>
2022-03-29 20:02:49 +0900

    utils: Change alpha and gamma

--- utils/util.py
+++ utils/util.py
@@ -14,7 +14,7 @@ def inverse_to_tensor_normalize(tensor: Tensor) -> Tensor:
     return tensor.mul_(255).to(torch.uint8)
 
 
-def draw_segmentation_masks(images: Tensor, masks: Tensor, colors: list, alpha=0.34, gamma=10) -> Tensor:
+def draw_segmentation_masks(images: Tensor, masks: Tensor, colors: list, alpha=0.4, gamma=20) -> Tensor:
     assert images.dtype == torch.uint8, f'The images dtype must be uint8, got {images.dtype}'
     assert images.dim() == 4, 'Pass batches, not individual images'
     assert images.size()[1] == 3, 'Pass RGB images. Other Image formats are not supported'
