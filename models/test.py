26c8cdd92919f0314f6bbac3b3b3606c34c25098
Seokyong Shin <kistssy@gmail.com>
2022-04-18 15:32:57 +0900

    models: Reformat code

--- models/test.py
+++ models/test.py
@@ -7,9 +7,13 @@ import torchinfo
 def test_model(model: nn.Module, input_size: tuple[int, int, int, int], graph_dir: str = None):
     model.eval()
 
-    model_statistics = torchinfo.summary(model, input_size, depth=10,
-                                         col_names=('input_size', 'kernel_size', 'output_size', 'num_params', 'mult_adds'),
-                                         row_settings=('depth', 'var_names'))
+    model_statistics = torchinfo.summary(
+        model,
+        input_size,
+        depth=10,
+        col_names=('input_size', 'kernel_size', 'output_size', 'num_params', 'mult_adds'),
+        row_settings=('depth', 'var_names')
+    )
     print(f'Total GFLOPs: {model_statistics.total_mult_adds * 2 / 1e9:.4f}')
 
     if graph_dir is not None:
