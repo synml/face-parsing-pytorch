87078210af9b648261a00f923f4870a5f2d7a103
Seokyong Shin <kistssy@gmail.com>
2022-04-19 09:09:00 +0900

    test: 기능 추가

--- models/test.py
+++ models/test.py
@@ -4,20 +4,28 @@ import torch.utils.tensorboard
 import torchinfo
 
 
-def test_model(model: nn.Module, input_size: tuple[int, int, int, int], graph_dir: str = None):
+def test_model(model: nn.Module, input_size: tuple[int, int, int, int] = None,
+               input_data: torch.Tensor | list[torch.Tensor] = None, graph_dir: str = None):
+    assert input_size is not None or input_data is not None, 'Either input_size or input_data must specify a value.'
+
     model.eval()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     model_statistics = torchinfo.summary(
         model,
         input_size,
+        input_data,
         depth=10,
+        device=device,
         col_names=('input_size', 'kernel_size', 'output_size', 'num_params', 'mult_adds'),
         row_settings=('depth', 'var_names')
     )
     print(f'Total GFLOPs: {model_statistics.total_mult_adds * 2 / 1e9:.4f}')
 
     if graph_dir is not None:
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         writer = torch.utils.tensorboard.SummaryWriter(graph_dir)
-        writer.add_graph(model, torch.rand(input_size, device=device))
+        if input_data is None:
+            writer.add_graph(model, torch.rand(input_size, device=device))
+        else:
+            writer.add_graph(model, input_data)
         writer.close()
