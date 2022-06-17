660a19cba8095e75e919acb50c3bc2eae8d366c9
Seokyong Shin <syshin@cubox.ai>
2022-06-17 13:34:26 +0900

    models: type hint 하위 호환성 추가

--- models/test.py
+++ models/test.py
@@ -2,10 +2,11 @@ import torch
 import torch.nn as nn
 import torch.utils.tensorboard
 import torchinfo
+from typing import Union
 
 
 def test_model(model: nn.Module, input_size: tuple[int, int, int, int] = None,
-               input_data: torch.Tensor | list[torch.Tensor] = None, graph_dir: str = None):
+               input_data: Union[torch.Tensor, list[torch.Tensor]] = None, graph_dir: str = None):
     assert input_size is not None or input_data is not None, 'Either input_size or input_data must specify a value.'
 
     model.eval()
